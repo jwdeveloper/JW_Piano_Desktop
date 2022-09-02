4eaea961f18740458d8d6cb75728ae5d54f5da69
JW <jacekwoln@gmail.com>
2022-09-02 21:56:16 +0200

    Keybaord input detection

--- src/main/java/jw/piano/Main.java
+++ src/main/java/jw/piano/Main.java
@@ -1,6 +1,21 @@
 package jw.piano;
 
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main
 {
